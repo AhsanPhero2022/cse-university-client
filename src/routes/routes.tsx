4805e60ff2d26234ec64ba3afcc6fe027f17e65b
Sharif Ahmed <97349171+AhsanPhero2022@users.noreply.github.com>
2024-02-10 21:01:10 +0600

    layout and routing system added

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,9 +1,47 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
+import About from "../pages/About";
+import Contact from "../pages/Contact";
+import AdminDashboard from "../pages/admin/AdminDashboard";
+import CreateStudent from "../pages/admin/CreateStudent";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+    children: [
+      {
+        path: "about",
+        element: <About />,
+      },
+      {
+        path: "contact",
+        element: <Contact />,
+      },
+    ],
+  },
+  {
+    path: "/admin",
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <AdminDashboard />,
+      },
+      {
+        path: "create-student",
+        element: <CreateStudent />,
+      },
+    ],
+  },
+  {
+    path: "login",
+    element: <Login />,
+  },
+  {
+    path: "register",
+    element: <Register />,
   },
 ]);
