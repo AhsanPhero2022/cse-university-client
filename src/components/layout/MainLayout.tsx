716e5a7a0f51a3714348f10734136a6adb22afba
Sharif Ahmed <97349171+AhsanPhero2022@users.noreply.github.com>
2024-02-12 07:44:32 +0600

    admin side item routes dynamical setup

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,32 +1,9 @@
 import { Layout, Menu } from "antd";
-import { NavLink, Outlet } from "react-router-dom";
+
+import { adminSidebarItems } from "../../routes/admin.routes";
+import { Outlet } from "react-router-dom";
 
 const { Header, Content, Footer, Sider } = Layout;
-
-const items = [
-  {
-    key: "Dashboard",
-    label: <NavLink to="/admin/dashboard">Dashboard</NavLink>,
-  },
-  {
-    key: "User Management",
-    label: "User Management",
-    children: [
-      {
-        key: "Create Admin",
-        label: <NavLink to="/admin/create-admin">Create Admin</NavLink>,
-      },
-      {
-        key: "Create Faculty",
-        label: <NavLink to="/admin/create-faculty">Create Admin</NavLink>,
-      },
-      {
-        key: "Create Student",
-        label: <NavLink to="/admin/create-student">Create Admin</NavLink>,
-      },
-    ],
-  },
-];
 
 const MainLayout = () => {
   return (
@@ -64,7 +41,7 @@ const MainLayout = () => {
           theme="dark"
           mode="inline"
           defaultSelectedKeys={["4"]}
-          items={items}
+          items={adminSidebarItems}
         />
       </Sider>
       <Layout>
--- test.js
+++ test.js
@@ -6,54 +6,51 @@ const result = arr.reduce((x, y) => {
 
 console.log(result);
 
-// type TRoute = {
-//   path: string,
-//   element: ReactNode,
-// };
+const adminPathsRouts2 = [
+  {
+    name: "Dashboard",
+    path: "/admin/dashboard",
+    element: "AdminDashboard",
+  },
+  {
+    name: "User Management",
+    children: [
+      {
+        name: "Create Admin",
+        path: "create-admin",
+        element: "CreateAdmin",
+      },
+      {
+        name: "Create Faculty",
+        path: "create-faculty",
+        element: "CreateFaculty",
+      },
+      {
+        name: "Create Student",
+        path: "create-student",
+        element: "CreateStudent",
+      },
+    ],
+  },
+];
 
-// const adminPathsRouts2 = [
-//   {
-//     name: "Dashboard",
-//     path: "/admin/dashboard",
-//     element: <AdminDashboard />,
-//   },
-//   {
-//     name: "User Management",
-//     children: [
-//       {
-//         name: "Create Admin",
-//         path: "create-admin",
-//         element: <CreateAdmin />,
-//       },
-//       {
-//         name: "Create Faculty",
-//         path: "create-faculty",
-//         element: <CreateFaculty />,
-//       },
-//       {
-//         name: "Create Student",
-//         path: "create-student",
-//         element: <CreateStudent />,
-//       },
-//     ],
-//   },
-// ];
+export const adminRoutes = adminPathsRouts2.reduce((acc, item) => {
+  if (item.path && item.name) {
+    acc.push({
+      key: item.path,
+      label: "Navlink",
+    });
+  }
 
-// export const adminRoutes = adminPathsRouts2.reduce((acc: TRoute[], item) => {
-//   if (item.path && item.name) {
-//     acc.push({
-//       path: item.path,
-//       element: item.element,
-//     });
-//   }
-
-//   if (item.children) {
-//     item.children.forEach((child) => {
-//       acc.push({
-//         path: child.path,
-//         element: child.element,
-//       });
-//     });
-//   }
-//   return acc;
-// }, []);
+  if (item.children) {
+    acc.push({
+      key: item.name,
+      label: "Navlink",
+      children: item.children.map((child) => ({
+        key: child.path,
+        label: "Navlink",
+      })),
+    });
+  }
+  return acc;
+}, []);
