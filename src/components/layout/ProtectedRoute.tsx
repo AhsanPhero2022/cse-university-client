095bdb3264186a1dc81eb8a2077410db4fa06fe7
Sharif Ahmed <97349171+AhsanPhero2022@users.noreply.github.com>
2024-02-14 07:27:09 +0600

    refactor al routes & sidbar

---
+++ src/components/layout/ProtectedRoute.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ProtectedRoute = () => {
+  return (
+    <div>
+      <h2>this is protected route</h2>
+    </div>
+  );
+};
+
+export default ProtectedRoute;
--- src/routes/admin.routes.tsx
+++ src/routes/admin.routes.tsx
@@ -1,21 +1,9 @@
-import { ReactNode } from "react";
 import AdminDashboard from "../pages/admin/AdminDashboard";
 import CreateAdmin from "../pages/admin/CreateAdmin";
 import CreateFaculty from "../pages/admin/CreateFaculty";
 import CreateStudent from "../pages/admin/CreateStudent";
-import { NavLink } from "react-router-dom";
 
-type TRoute = {
-  path: string;
-  element: ReactNode;
-};
-
-type TAdminSideItem = {
-  key: string;
-  label: ReactNode;
-  children?: TAdminSideItem[];
-};
-const adminPathsRouts2 = [
+export const adminPaths = [
   {
     name: "Dashboard",
     path: "dashboard",
@@ -39,49 +27,11 @@ const adminPathsRouts2 = [
         path: "create-student",
         element: <CreateStudent />,
       },
+      {
+        name: "Create Member",
+        path: "create-member",
+        element: <CreateStudent />,
+      },
     ],
   },
 ];
-
-export const adminRoutes = adminPathsRouts2.reduce((acc: TRoute[], item) => {
-  if (item.path && item.name) {
-    acc.push({
-      path: item.path,
-      element: item.element,
-    });
-  }
-
-  if (item.children) {
-    item.children.forEach((child) => {
-      acc.push({
-        path: child.path,
-        element: child.element,
-      });
-    });
-  }
-  return acc;
-}, []);
-
-export const adminSidebarItems = adminPathsRouts2.reduce(
-  (acc: TAdminSideItem[], item) => {
-    if (item.path && item.name) {
-      acc.push({
-        key: item.path,
-        label: <NavLink to={`/admin/${item.path}`}>{item.name}</NavLink>,
-      });
-    }
-
-    if (item.children) {
-      acc.push({
-        key: item.name,
-        label: item.name,
-        children: item.children.map((child) => ({
-          key: child.path,
-          label: <NavLink to={`/admin/${child.path}`}>{child.name}</NavLink>,
-        })),
-      });
-    }
-    return acc;
-  },
-  []
-);
